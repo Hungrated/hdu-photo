bcadd742f8fc9f2cd801dabcba3f9f96cee17ddd
Hungrated <zhang295415658@qq.com>
2017-06-27 22:43:06 +0800

    Basic programming - 2

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,3 +1,15 @@
 /**
  * Created by Zihang Zhang on 2017/6/27.
  */
+
+$(document).ready(function () {
+    $("#header-index").click(function () {
+        window.location.href = 'http://localhost:63342/hdu_photo/public/index.html';
+    });
+    $("#header-portfolio").click(function () {
+        window.location.href = 'http://localhost:63342/hdu_photo/public/index.html';
+    });
+    $("#header-gallery").click(function () {
+        window.location.href = 'http://localhost:63342/hdu_photo/public/gallery.html';
+    });
+});
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,35 @@
 /**
  * Created by Zihang Zhang on 2017/6/27.
  */
+
+$(document).ready(function () {
+    var wrap = (function() {
+        var target;
+        var nowActive;
+        var nextActive;
+        var nextnextActive;
+        var init = function(dom) {
+            target = $(dom);
+            listen();
+        };
+        var listen = function() {
+            var start = function(resolve) {
+                nowActive = target.find('.active');
+                nextActive = $('.item' + nowActive.attr('data-next'));
+                nextnextActive = $('.item' + nextActive.attr('data-next'));
+                nowActive.addClass('left');
+                nextActive.addClass('right');
+                setTimeout(function() {
+                    nowActive.removeClass('active').removeClass('left');
+                    nextActive.removeClass('next').removeClass('right').addClass('active');
+                    nextnextActive.addClass('next');
+                }, 1000);
+            };
+            setInterval(start, 4000);
+        };
+        return {
+            init: init
+        }
+    })();
+    wrap.init(".m-wrap");
+});
